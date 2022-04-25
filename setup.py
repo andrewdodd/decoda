81ea2aaf986e5f91c09994cf405d71f3b229f58a
Andrew Dodd <andrew.john.dodd@gmail.com>
2022-04-25 19:27:19 +1000

    Change `setup.py` to use Hynek's technique
    These changes:
     - Move a bunch of metadata into the `decoda/__init__.py` file
     - Use Hynek's technique for setup.py, including reading from these meta
       file

--- setup.py
+++ setup.py
@@ -1,49 +1,110 @@
+import codecs
+import os
+import re
+
 from setuptools import find_packages, setup
 
-setup(
-    name="decoda",
-    description="Copyright Andrew Dodd",
-    license="Nil",
-    version="1.1.0",
-    author="Andrew Dodd",
-    author_email="andrew.john.dodd@gmail.com",
-    maintainer="Andrew Dodd",
-    maintainer_email="andrew.john.dodd@gmail.com",
-    keywords=["decoda", "j1939"],
-    packages=find_packages(
-        where="src",
-        exclude=["doc", "reference"],
+###############################################################################
+
+NAME = "decoda"
+PACKAGES = find_packages(where="src")
+META_PATH = os.path.join("src", "decoda", "__init__.py")
+KEYWORDS = ["j1939"]
+PROJECT_URLS = {
+    "Documentation": "https://www.decoda.cc/",
+    "Source Code": "https://github.com/andrewdodd/decoda",
+}
+CLASSIFIERS = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "Natural Language :: English",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+INSTALL_REQUIRES = [
+    "attrs",
+    # Installing pretty_j1939 instead of its dependencies, even
+    # though we don't really use it directly (i.e. is it here just so
+    # the copied create_j1939db_json.py file works)
+    "pretty_j1939",
+    "xlrd2",
+]
+# I'm not really sure what this is for
+EXTRAS_REQUIRE = {}
+
+###############################################################################
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    """
+    Build an absolute path from *parts* and return the contents of the
+    resulting file.  Assume UTF-8 encoding.
+    """
+    with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f:
+        return f.read()
+
+
+META_FILE = read(META_PATH)
+
+
+def find_meta(meta):
+    """
+    Extract __*meta*__ from META_FILE.
+    """
+    meta_match = re.search(
+        r"^__{meta}__ = ['\"]([^'\"]*)['\"]".format(meta=meta), META_FILE, re.M
     )
-    + ["tests"],
-    package_dir={"": "src", "tests": "./tests"},
-    zip_safe=False,
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "DO NOT UPLOAD to PyPI",
-    ],
-    install_requires=[
-        "attrs",
-        # Installing pretty_j1939 instead of its dependencies, even
-        # though we don't really use it directly (i.e. is it here just so
-        # the copied create_j1939db_json.py file works)
-        "pretty_j1939",
-        "xlrd2",
-    ],
-    entry_points={
-        "console_scripts": [
-            "json_from_digital_annex=decoda.sae_spec_converter.json_from_da:main",
-            "json_from_isobus_xlsx=decoda.sae_spec_converter.json_from_isobus_xlsx:main",
-            "enrich_spec=decoda.sae_spec_converter.enrich_spec:main",
-            "correct_spec=decoda.sae_spec_converter.correct_spec:main",
-            "remove_bad_items=decoda.sae_spec_converter.remove_bad_items:main",
-        ]
-    },
-)
+    if meta_match:
+        return meta_match.group(1)
+    raise RuntimeError("Unable to find __{meta}__ string.".format(meta=meta))
+
+
+VERSION = find_meta("version")
+URL = find_meta("url")
+LONG = read("README.md")
+
+
+if __name__ == "__main__":
+    setup(
+        name=NAME,
+        description=find_meta("description"),
+        license=find_meta("license"),
+        url=URL,
+        project_urls=PROJECT_URLS,
+        version=VERSION,
+        author=find_meta("author"),
+        author_email=find_meta("email"),
+        maintainer=find_meta("author"),
+        maintainer_email=find_meta("email"),
+        keywords=KEYWORDS,
+        long_description=LONG,
+        long_description_content_type="text/markdown",
+        packages=PACKAGES,
+        package_dir={"": "src"},
+        python_requires=">=3.6",
+        zip_safe=False,
+        classifiers=CLASSIFIERS,
+        install_requires=INSTALL_REQUIRES,
+        extras_require=EXTRAS_REQUIRE,
+        include_package_data=True,
+        options={"bdist_wheel": {"universal": "1"}},
+        entry_points={
+            "console_scripts": [
+                "json_from_digital_annex=decoda.sae_spec_converter.json_from_da:main",
+                "json_from_isobus_xlsx=decoda.sae_spec_converter.json_from_isobus_xlsx:main",
+                "enrich_spec=decoda.sae_spec_converter.enrich_spec:main",
+                "correct_spec=decoda.sae_spec_converter.correct_spec:main",
+                "remove_bad_items=decoda.sae_spec_converter.remove_bad_items:main",
+            ]
+        },
+    )
--- src/decoda/__init__.py
+++ src/decoda/__init__.py
@@ -1,4 +1,17 @@
-# Copyright Andrew Dodd
 from .main import *
 from .spec_loader import *
 from .transport import *
+
+__version__ = "0.1.0.dev0"
+
+__title__ = "decoda"
+__description__ = "Decoder for J1939"
+__url__ = "https://www.decoda.cc/"
+__uri__ = __url__
+__doc__ = __description__ + " <" + __uri__ + ">"
+
+__author__ = "Andrew Dodd"
+__email__ = "andrew.john.dodd@gmail.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) Andrew Dodd"
