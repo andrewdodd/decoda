640bd26b883cefc0e767498997927ef51782d404
Andrew Dodd <andydodd@amazon.com>
2024-10-15 19:35:03 +1100

    Move to 0.2.6.dev0

--- src/decoda/__init__.py
+++ src/decoda/__init__.py
@@ -2,7 +2,7 @@ from .main import *
 from .spec_loader import *
 from .transport import *
 
-__version__ = "0.2.5"
+__version__ = "0.2.6.dev0"
 
 __title__ = "decoda"
 __description__ = "Decoder for J1939"
